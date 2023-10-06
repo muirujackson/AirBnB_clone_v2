47dec9fc88445264f0af1e9d3bd0a7998e4a9302
muirujackson <jakezweru@gmail.com>
2023-10-06 15:44:07 -0700

    update do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to your web servers
+With Facric , creates a tgz archive
+from web_static content folder
 """
 
-from datetime import datetime
-from fabric.api import *
-import os
+from fabric.api import env, put, run
+from os.path import exists
+env.hosts = ['52.87.216.130', '52.91.133.43']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
-env.hosts = ["54.197.45.60", "18.207.235.240"]
-env.user = "ubuntu"
+def do_deploy(archive_path):
+    """deploy web static with fabric"""
+    if exists(archive_path) is False:
+        return False
 
-
-def do_pack():
-    """
-        return the archive path if archive has generated correctly.
-    """
-
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archived_f_path = "versions/web_static_{}.tgz".format(date)
-    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
-
-    if t_gzip_archive.succeeded:
-        return archived_f_path
-    else:
-        return None
+    try:
+        filename = archive_path.split("/")[-1]
+        no_excep = filename.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('sudo mkdir -p {}{}/'.format(path, no_excep))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(filename, path, no_excep))
+        run('sudo rm /tmp/{}'.format(filename))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_excep))
+        run('sudo rm -rf {}{}/web_static'.format(path, no_excep))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_excep))
+        return True
+    except BaseException:
+        return False
