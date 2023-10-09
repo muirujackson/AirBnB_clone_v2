11befa94a68443fe17f819c4527f2ac153e8d800
muirujackson <jakezweru@gmail.com>
2023-10-09 06:52:04 -0700

    pycodestyle added

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"clean previous release and keep the latest"
+
 import os
 from fabric.api import *
 
 env.hosts = ['52.91.133.43', '52.87.216.130']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
+
 
 def do_clean(number=0):
     """Delete out-of-date archives.
@@ -16,8 +19,7 @@ def do_clean(number=0):
     number is 2, keeps the most and second-most recent archives,
     etc.
     """
-    number = 1 if int(number) == 0  else int(number)
-
+    number = 1 if int(number) == 0 else int(number)
     archives = sorted(os.listdir("versions"))
     [archives.pop() for i in range(number)]
     with lcd("versions"):
