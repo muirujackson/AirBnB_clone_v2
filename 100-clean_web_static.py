b976994e585b5ba1070439b7915f28c95dde7932
muirujackson <jakezweru@gmail.com>
2023-10-09 06:43:38 -0700

    added permission

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -21,10 +21,10 @@ def do_clean(number=0):
     archives = sorted(os.listdir("versions"))
     [archives.pop() for i in range(number)]
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+        [local("sudo rm ./{}".format(a)) for a in archives]
 
     with cd("/data/web_static/releases"):
         archives = run("ls -tr").split()
         archives = [a for a in archives if "web_static_" in a]
         [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        [run("sudo rm -rf ./{}".format(a)) for a in archives]
