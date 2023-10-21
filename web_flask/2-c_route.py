289f6e45a38112dbf17ce2d766302cdc1a18ab72
muirujackson <jakezweru@gmail.com>
2023-10-21 01:09:59 -0700

    added variable rules

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,6 +3,7 @@
 
 
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -12,6 +13,16 @@ def hello_hbnb():
     """ This function return a hello message """
     return "Hello HBNB!"
 
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ This method diplay 'HBNB' """
+    return "HBNB"
+
+@app.route('/c/<text>', strct_slashe=False)
+def ctext(text):
+    """ this method 'c' followed by value of text """
+    return f"C {escape(text)}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
