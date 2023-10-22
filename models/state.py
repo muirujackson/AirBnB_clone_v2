bc0ccad86e3476bd578271d20043d6fe38cf5405
muirujackson <jakezweru@gmail.com>
2023-10-22 09:29:48 -0700

    added pycode style

--- models/state.py
+++ models/state.py
@@ -7,12 +7,13 @@ from sqlalchemy.orm import relationship
 from models.base_model import Base, BaseModel
 from models.city import City
 
+
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    
+
     if getenv("HBNB_TYPE_STORAGE") == 'db':
         cities = relationship(
             'City',
