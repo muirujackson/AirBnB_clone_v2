b268a890f7e41a2acb6369cd6307ecf249a315d6
muirujackson <jakezweru@gmail.com>
2023-10-22 13:00:45 -0700

    get data from data base

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application
+web application must be listening on 0.0.0.0, port 5000
+fetching data from the storage engine
+"""
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """Closes the current SQLAlchemy session."""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a list of State objects from the database."""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+# Configure the application to listen on 0.0.0.0 and port 5000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
