947b5f603d46162837369a5dc7d6839bc1bcf030
muirujackson <jakezweru@gmail.com>
2023-10-22 02:21:04 -0700

    send a html with route

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" create a script that starts a Flask web application """
+
+
+from markupsafe import escape
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ This function return a hello message """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ This method diplay 'HBNB' """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """ Replace underscores with spaces in the 'text' variable """
+    text = text.replace('_', ' ')
+    return f"C {escape(text)}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def d_python(text="is cool"):
+    """
+    This diplate python plus value of text
+    underscore in text are replace with space
+    """
+    text = text.replace('_', ' ')
+    return f"Python {escape(text)}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+        return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_number_template(n):
+    if isinstance(n, int):
+        return render_template('number_template.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
